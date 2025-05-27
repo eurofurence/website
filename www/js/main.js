a31b65438ba8ca9ea4b46ae71fc26eed3d98ce00
draconigen <draconigen@dogpixels.net>
2025-05-27 22:38:56 +0200

    chore: :children_crossing: add additional nav bar behaviour

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,7 @@
 // introduced by Xenor in 2023: make the background page unscrollable while the nav overlay is open
-document.getElementById("nav-state").addEventListener('change', function() {
-    document.getElementsByTagName("body")[0].style = "overflow: " + (this.checked? "hidden" : "auto") + ";"
-})
+// document.getElementById("nav-state").addEventListener('change', function() {
+//     document.getElementsByTagName("body")[0].style = "overflow: " + (this.checked? "hidden" : "auto") + ";"
+// })
 
 // job deeplinking
 document.querySelectorAll('[id^=ef-job-]').forEach(element => {
@@ -19,3 +19,23 @@ const job = document.getElementById(`ef-job-${window.location.hash.substring(1)}
 if (window.location.hash && job) {
     UIkit.modal(job).show();
 }
+
+// dropdown navigation bar
+document.querySelectorAll('nav label[for^="subnav-"]').forEach(label => {
+    // close previously opened nav dropdown on repeated click
+    let radio = document.getElementById(label.htmlFor);
+    label.addEventListener('click', (event) => {
+        if (radio.checked) {
+            radio.checked = false;
+            event.preventDefault();
+        }
+    });
+    // or on hovering another nav item
+    label.addEventListener('mouseenter', (event) => {
+        document.querySelectorAll('nav input[id^="subnav-"]').forEach(r => {
+            if (r.id != label.htmlFor) {
+                r.checked = false;
+            }
+        })
+    });
+});
