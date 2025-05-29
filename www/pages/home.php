417d2e2a2f67526753492caa558924ba96fe2fc9
draconigen <draconigen@dogpixels.net>
2025-05-30 00:31:33 +0200

    feat: :sparkles: landing page: add photo

--- www/pages/home.php
+++ www/pages/home.php
@@ -7,6 +7,15 @@
     </p>
 </div>
 
+<div id="ef-home-photo">
+    <p>
+        Lorem ipsum dolor sit amet adipiscing et te duo dolores justo sed consequat feugiat amet elitr amet at amet lorem et et et. Stet accusam eum dolores no duo ipsum sit clita illum aliquyam nonumy tempor elit sadipscing lorem amet. Cum justo possim kasd et et duo elitr suscipit et. Magna facilisis dolor diam et qui ipsum dolores. Dolor facilisi justo qui magna justo amet tempor.
+        <br /><br />
+        <a href="https://identity.eurofurence.org/" class="uk-button uk-button-primary" target="_blank">REGISTER NOW</a>
+    </p>
+</div>
+
+
 <div class="uk-margin-top">
     <a href="#content" id="scrolldown" uk-scroll="offset: 50"><strong>Scroll Down<br /><span uk-icon="icon: chevron-down"></span></strong></a>
 
