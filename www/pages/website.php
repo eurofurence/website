d123a08e181b122ff68c64c62ed99c242416f103
draconigen <draconigen@dogpixels.net>
2025-11-09 15:46:22 +0100

    re-arrange navigation

--- www/pages/website.php
+++ www/pages/website.php
@@ -12,20 +12,29 @@
 
 <hr />
 
-<section id="ef-badger" class="uk-column-1-2@l">
-	<div>
-		<h2>Eurofurence Badger Addon</h2>
-		<p>This brand-new browser addon will enhance your visit to the Eurofurence website by adding <span class="ef-new"></span> badges to pages that have been updated since you last viewed them!</p>
-		<p>Available for <a href="https://addons.mozilla.org/firefox/addon/eurofurence-badger/" target="_blank">Mozilla Firefox</a> and <a href="https://chrome.google.com/" target="_blank">Google Chrome</a>.</p>
-	</div>
-	<div>
-		<h2>Third Party Attributions</h2>
-		<ul>
-			<li><a href="https://getuikit.com" target="_blank">UIkit</a> by <a href="https://yootheme.com/" target="_blank">YOOtheme GmbH</a> (<a href="https://github.com/uikit/uikit/blob/develop/LICENSE.md" target="_blank">license</a>)</li>
-			<li><a href="" target="_blank">Mastodon Embed Timeline Widget</a> by i.j (<a href="https://gitlab.com/idotj/mastodon-embed-timeline/" target="_blank">source</a>, <a href="https://gitlab.com/idotj/mastodon-embed-timeline/-/blob/master/LICENSE" target="_blank">license</a>)
-		</ul>
-	</div>
+<h2>Banner Exchange</h2>
+<section>
+	<p>Dear Convention Owner,<br />you want your convention to appear in the partners slide on the bottom of our website? <a href="https://help.eurofurence.org/contact/web" target="_blank">Drop us a message</a>.
 </section>
+
+<section>
+	<h3>We will need two things from you:</h3>
+	<ul>
+		<li>Our banner on your website. You'll find our file under the permanent URL <strong>https://www.eurofurence.org/ef_banner.gif</strong> - When placing it onto your page, feel free to use this direct link. Our hosting is very stable, and very unlikely to disappear anytime in the far future. Plus, we'll eventually maintain a more theme-specific picture according to the annual convention theme.</li>
+		<li>Your banner. It should be 200x80 px in size. Static pictures are preferred, but not a must. If you decide to animate your banner, please don't make it too annoying or load-heavy. We reserve the right to reject your submission if we find it too flashy, quick-changing, unfitting or exceedingly big as a file; In such case, we'd get back at you for a proper version, of course.</li>
+	</ul><br/>
+	<h3>There are two ways to submit your banner:</h3>
+	<ol>
+		<li><strong>Preferred:</strong> You give us an URL, under which your banner is going to be permanently available. That way, whenever you decide to change your banner (e.g. because it's theme-based), it'll change on our website as well. Downside: You'll have to make sure that your banner really IS available - preferably forever! We periodically (every 14 days) check all banners. If yours is no longer reachable it will temporarily be removed from the slider. If it is updated, the new one will show after the next check-cycle - no need to notify us of updates. Also don't worry: We cache the banners and only access them every 2 weeks for re-checking, so no permanent access to your website is made.</li>
+		<li><strong>Fallback:</strong> You send us a file. Done. Easy as that.</li>
+	</ol>
+</section>
+
+<section>
+	<br/>And this is how our current banner looks like: <br/><br/>
+	<img class="framedimage" src="https://www.eurofurence.org/ef_banner.gif" alt="Eurofurence Banner" /><br/><br/>
+</section>
+
 
 <hr />
 
