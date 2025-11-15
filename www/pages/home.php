08ff219ea546d3f1d31941bc66791863f18b6345
draconigen <draconigen@dogpixels.net>
2025-11-15 16:57:55 +0100

    fix intro buttons clinging on small displays

--- www/pages/home.php
+++ www/pages/home.php
@@ -69,10 +69,10 @@
         <p>Unlike other cons we have a whole convention centre and over 100 hotels in the city to choose from</p>
         <p>this makes a con like no other, your friends will be there so what's stopping you?</p>
         
-        <p class="uk-margin-remove-bottom">
-            <a href="about" class="uk-button uk-button-primary" target="_blank">LEARN MORE</a>
-            <a href="https://identity.eurofurence.org/" class="uk-button uk-button-primary" target="_blank">REGISTER NOW</a>
-        </p>
+        <div class="uk-grid-small" uk-grid>
+            <div><a href="about" class="uk-button uk-button-primary" target="_blank">LEARN MORE</a></div>
+            <div><a href="https://identity.eurofurence.org/" class="uk-button uk-button-primary" target="_blank">REGISTER NOW</a></div>
+        </div>
     </div>
     <div id="ef-home-photos" tabindex="-1" uk-slideshow="ratio: 1280:400; autoplay: true; autoplay-interval: 5000">
         <div class="uk-slideshow-items">
