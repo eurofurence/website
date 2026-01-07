f299e7114d53678bbebcf255ebd9b580a0e02899
draconigen <draconigen@dogpixels.net>
2026-01-07 13:34:07 +0100

    logistics: conops -> own department

---
+++ www/pages/jobs/logistics-helper.php
@@ -0,0 +1,28 @@
+<?php
+    $frontmatter["department"] = "Logistics";
+    $frontmatter["title"] = "Helper";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>Join the Logistics Team at Eurofurence!</p>
+<p>Do you want to be a key player in making Eurofurence a success? Our Logistics team is the backbone of the convention, ensuring everything is where it needs to be, exactly when it’s needed!</p>
+<p>What We Do:</p>
+<ul>
+    <li>Gathering Supplies: Collect all necessary consumables from various wholesalers.</li>
+    <li>Hardware Pickups: Retrieve essential items from the hardware store.</li>
+    <li>Transport: Move equipment between different storage locations and the convention site.</li>
+    <li>Order Handling: Manage material orders from various departments.</li>
+    <li>Route Planning: Strategize and sort out the most efficient delivery routes.</li>
+</ul>
+<p>We ensure that all other departments have what they need to make Eurofurence an unforgettable experience!</p>
+<p>Key Work Periods:</p>
+<ul>
+    <li>Early Arrival Day: Most of our work happens the day before the convention (Early Arrival Day) and 1-2 days after it ends (Late Departure).</li>
+</ul>
+<p>What We’re Looking For:</p>
+<ul>
+    <li>Experience: Some background in logistics is recommended.</li>
+    <li>Driving Skills: Owning a truck driving license is highly preferred but not mandatory.</li>
+    <li>Team Spirit: Willingness to collaborate and take part in meetings weeks before the convention.</li>
+</ul>
+<p>Interested in Making Eurofurence Happen? Visit us during the convention at our Post/Logistics Office, or drop us a message at <a href="mailto:logistics@eurofurence.org">logistics@eurofurence.org</a>.</p>
