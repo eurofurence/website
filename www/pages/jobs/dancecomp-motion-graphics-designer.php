6c553628a3089fef5b03b9e08428e0850deca457
draconigen <draconigen@dogpixels.net>
2026-01-21 22:06:53 +0100

    jobs/dancecomp: add motion graphics designer

---
+++ www/pages/jobs/dancecomp-motion-graphics-designer.php
@@ -0,0 +1,13 @@
+<?php
+    $frontmatter["department"] = "Dance Competitions";
+    $frontmatter["title"] = "Motion Graphics Designer";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>For the two shows our team produces (Enter the Arena and Paws on Fire) we are looking for a Motion Graphics Designer!</p>
+<p>During our shows, we have the availability to use the television-screens on stage and the live-stream to enrich the experience for the viewers and the dancers, by generating additional eye-candy animations!</p>
+<p>Working with animation programs like Premiere Pro, After Effects or alternatives, to design and produce short animations which the backstage team will play on site.</p>
+<p>Together with the producers of this team, you can work out a themed design. Inspired by almost 40+- dancers, we can together work out the motion graphics designs we want during the shows like logo loops, name and title bars, moving pictures, Start The Dance effects and end of the show extravaganza!</p>
+<p>The second team you’ll be talking lots with is the backstage team, to see what’s possible and make a script for the animations.</p>
+<p>You’ll be part of a fun team of Eurofurence, who’s sole focus is to produce two dance contests. Most of this particular job is worked on the months before packing your bags for the con, during the con you can sit with the backstage team during the two shows to see your work used for the entire crowd.</p>
+<p>For applying for the job, please contact @<a href="https://t.me/@Wolvinny" target="_blank">Wolvinny over at telegram</a>.</p>
