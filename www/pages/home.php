748c8a12d3615867344a796c9a68a4b3e1b14afe
draconigen <draconigen@dogpixels.net>
2026-01-24 13:23:48 +0100

    about: disable page

--- www/pages/home.php
+++ www/pages/home.php
@@ -60,7 +60,7 @@ iframe {
         
         <div class="uk-grid-small" uk-grid>
             <div><a href="https://identity.eurofurence.org/" class="uk-button hide-ext uk-button-primary" target="_blank">REGISTER NOW</a></div>
-            <div><a href="about" class="uk-button uk-button-primary" target="_blank">LEARN MORE</a></div>
+            <!-- <div><a href="about" class="uk-button uk-button-primary" target="_blank">LEARN MORE</a></div> -->
         </div>
     </div>
     <div id="ef-home-photos" tabindex="-1" uk-slideshow="ratio: 1280:400; autoplay: true; autoplay-interval: 5000">
