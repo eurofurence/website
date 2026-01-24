6e1fc0573befd02c2120992317c86de2dd5f295f
draconigen <draconigen@dogpixels.net>
2026-01-24 14:01:25 +0100

    jobs/goh: add liaison

---
+++ www/pages/jobs/goh-members.php
@@ -0,0 +1,22 @@
+<?php
+    $frontmatter["department"] = "Guest of Honor";
+    $frontmatter["title"] = "Liaison";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>The Guest of Honor Department is looking for new members for the On-Site Team!</p>
+
+<p>To make sure our Guest of Honors have the best time possible at Eurofurence and attendees enjoying their Panels we need new Team members with the following qualifications:</p>
+
+<ul>
+    <li>Fluent in English</li>
+    <li>professional behavior towards the Guests of Honor</li>
+    <li>able to 'jump in' on short notice to prep for Panels or anything the GoH needs</li>
+    <li>able to troubleshoot Problems that arise during Panels </li>
+    <li>represent the Department on stage (Opening and/or Closing ceremony)</li>
+</ul>
+
+<p>The Panels will be mainly hold in the evening/night if possible, so the Team members have to be reachable in those hours.</p>
+<p>Other activities regarding the GoH may be hold during the day though. All Availability Times can be planed beforehand.</p>
+
+<p>Interested? Then contact the Department <a href="https://tm.me/@Nerventee" target="_blank">@Nerventee via Telegram</a>!</p>
