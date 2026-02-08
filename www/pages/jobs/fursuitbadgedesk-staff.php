fa22be72c1b0a65fe5c4d07dff939b4795629d5b
draconigen <draconigen@dogpixels.net>
2026-02-08 20:15:51 +0100

    jobs/fursuitbadgedesk-staff:  restore from EF29

---
+++ www/pages/jobs/fursuitbadgedesk-staff.php
@@ -0,0 +1,43 @@
+<?php
+    $frontmatter["department"] = "Fursuit Badge Desk";
+    $frontmatter["title"] = "Staff";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>At the Fursuit Badge Desk, you’ll be part of the team that makes one of the most beloved Eurofurence traditions possible: the fursuit badge. From handing out badges and explaining  the catch-em-all game to attendees, to producing new badges on-site and capturing that magical moment when a fursuiter beams with pride in front of your camera — this role puts you right in the middle of the joy and creativity of the con.</p>
+
+<p>Our desk is located directly in the Fursuit Lounge, so you’ll be at the heart of the action where suiters prepare, cool down, and meet friends. If you love fursuits and want to see them all while helping, this is the team for you!</p>
+
+<h3>Your Responsibilities</h3>
+
+<p>As Fursuit Badge Desk Staff, you will:</p>
+
+<ul>
+    <li><strong>Badge Issuing:</strong> Hand out pre-registered badges to fursuiters and guide them through the process with a smile.</li>
+    <li><strong>On-Site Badge Creation:</strong> Produce new badges by taking photos, processing them, and printing professional-quality badges.</li>
+    <li><strong>Photography:</strong> Capture quick but joyful portraits of fursuiters, helping them look their best on their fursuit badge.</li>
+    <li><strong>Handling Payments:</strong> Collect cash and process payments for on-site badge production.</li>
+    <li><strong>Game Support:</strong> Be part of the team that supports the popular <em>Catch ’Em All</em> game, where codes printed on fursuit badges can be collected by attendees.</li>
+    <li><strong>Problem-Solving:</strong> Handle common issues (like lost badges or technical hiccups) and escalate more complex cases to your lead.</li>
+</ul>
+
+<h3>Your Profile</h3>
+
+<p>We are looking for people who bring:</p>
+
+<p>Availability:</p>
+
+<ul>
+    <li>Able to commit several hours of volunteer time during Eurofurence.</li>
+    <li>Present on-site from day 1 through day 5 of the convention.</li>
+    <li>People Skills: Friendly, patient, and approachable — someone who can put attendees at ease, even when the line is long.</li>
+</ul>
+
+<p>Communication:</p>
+
+<ul>
+    <li>Fluency in English is required to clearly communicate with attendees.</li>
+    <li>Be able to work in a potentially loud environment, with fans running, fursuiters talking and printers roaring.</li>
+</ul>
+
+<p>If you are interested in joining contact <a href="https://t.me/Thiritin" target="_blank">@Thiritin</a> on Telegram!</p>
