9cce9c6f334a35f042d218a03cef147c0c1fd1a0
draconigen <draconigen@dogpixels.net>
2026-02-15 20:42:45 +0100

    conbook: update contents

--- www/pages/conbook.php
+++ www/pages/conbook.php
@@ -1,8 +1,22 @@
-<h1>Conbook</h1>
+<?php $deadline = 'June 19th'; ?>
 <section>
+	<h1>Conbook</h1>
 	<p>As every year, we will have a Conbook as guide through the convention and its events, as map, and as souvenir. If you're interested in contributing content, here are the guidelines.</p>
-	<p>All contributions, requests, and notes should be sent to our <a href="https://help.eurofurence.org/contact/conbook" target="_blank">Conbook department</a>.
-	<p><b>Deadline:</b> TBA</p>
+	
+	<b>Upload</b>
+	<p>
+		If you want to send us your artwork, stories or question please use the <a href="https://help.eurofurence.org/contact/conbook" target="_blank"><b>contact form</b></a>.
+		Share your pictures via your own website, a cloud service or a gallery which does not need a login.
+		Make sure it has a good resolution and quality for printing following the instructions further below.
+	</p>
+	
+	<p><b>Deadline:</b> <?= $deadline ?></p>
+
+	<b>Theme Guideline</b>
+	<p>
+		Need some help and inspiration with this years theme? Check out our <a href="https://cloud.eurofurence.org/index.php/s/6EZJfG8d2EqpGR5?dir=/&openfile=true" target="_blank"><b>visual guide</b></a>. 
+		If you have some unique ideas in how to interpret the theme we encourage this of course! This is only meant as a rough orientation - Let them guide you, then make it your own.
+	</p>
 </section>
 
 <section>
@@ -10,30 +24,31 @@
 	<h3 class="uk-margin-remove-top">(If you're an artist, this one is for you!)</h3>
 	<ul class="uk-column-1-2@l">
 		<li>The Conbook is in full color, so we prefer color illustrations as well.</li>
-		<br/>
+
 		<li>We are looking for full page illustrations as well as smaller fillers.</li>
-		<br/>
+
 		<li>The format for full page illustrations is A4, upright (210 x 297mm) plus 3mm cropping area along all edges, adding up to a total of 216 x 303 mm or 2551 x 3578 pixels at 300 dpi.<br /><br />
 			<img src="img/pages/conbook/How-to-conbook_1.jpg" alt="How to Conbook" />
 		</li>
-		<br/>
 		<li>File format should be JPG set to highest quality (low information loss). Other formats are acceptable too but may cause issues due to the resulting file size.</li><br/>
 		<li>For a full page illustration, do not draw anything important into the cropping area. Please avoid symmetrical motifs or motifs that rely on exact cutting of the page &ndash; for technical reasons, the cropping of the finished book may occur anywhere in the cropping area of each page.</li><br/>
-		<li>Fillers that do not have an illustrated background (extracted cutouts) need to have a fully transparent image background, since the pages themselves will have their own color background. The foreground needs to be intransparent (opaque). Be careful about holes in the foreground (transparent) and white foreground elements (opaque)!<br /><br />
-			<img src="img/pages/conbook/How-to-conbook_2.jpg" alt="How to Fillers" /> 
+		<li>Fillers that do not have an illustrated background (extracted cutouts) need to have a fully transparent image background, since the pages themselves will have their own color background. The foreground needs to be intransparent (opaque). Be careful about holes in the foreground (transparent) and white foreground elements (opaque)!
+			<img src="img/pages/conbook/How-to-conbook_2.jpg" alt="How to Fillers" class="uk-margin uk-margin-top" /> 
 		</li>
-		<br/>
+
 		<li>To submit illustrations that contain transparency, please use the PNG format.</li>
-		<br />
+
 		<li>The submission must NOT exceed PG-13 rating.</li>
-		<br />
+
 		<li>The artists for the cover art work and the centerfold will be chosen and approached directly by the team.</li>
+
+		<li>Sometimes we can include horizontal pictures in the Conbook but usually these spots are not planned in advance. If you have something that fits the theme you can still send it in. So we might only be able to show a part of it or use it as a background image.</li>
 	</ul>
 	
 	<hr />
 
 	<div class="uk-column-1-2@l">
-		<p>While there is no ongoing contest (the book's cover and centerfold have been assigned), there's still a challenge in getting into the book! Acceptance depends on quality, total number of images submitted, layout requirements, and the implementation of the con's topic! We will not be able to tell you immediately whether your submission makes the cut; this can be decided only in the final layout, so please be patient.</p>
+		<p>While there is no ongoing contest, there's still a challenge in getting into the book! Acceptance depends on quality, total number of images submitted, layout requirements, and the implementation of the con's topic! We will not be able to tell you immediately whether your submission makes the cut; this can be decided only in the final layout, so please be patient.</p>
 		<p>If you have graphic ideas other than illustrations which fit with the topic, you're welcome to submit them as well!</p>
 		<p>Even if your artwork does not get featured in the Conbook itself, we will always try to include it if applicable in various media across the convention (e.g. banners, dance visuals ect.)</p>
 		<p>Please provide following information along with your submission:</p>
@@ -48,6 +63,10 @@
 </section>
 
 <section>
-	<h2>Texts</h2>
-	<p>Due to space issues, we currently do not include stories or articles with the Conbook.</p>
+	<h2>Short Stories</h2>
+	<p>
+		We would love to include a couple of short stories, poems and similar again! 
+		The story has to be written in English, a theme fitting speech style can be used. 
+		Word count for short stories: Roughly a full or half page so 300-500 words.
+	</p>
 </section>
