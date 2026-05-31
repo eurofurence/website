241dd696a2a551af3a53c99dc59413d1baae9250
draconigen <draconigen@dogpixels.net>
2026-05-31 12:16:28 +0200

    jobs: add theming light tech

---
+++ www/pages/jobs/theming-light-tech.php
@@ -0,0 +1,50 @@
+<?php
+    $frontmatter["department"] = "Theming & Experience";
+    $frontmatter["title"] = "Light Technician";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>
+    The Eurofurence Theming and Experience Team designs one of Europe’s largest furry conventions. The team consists of dedicated volunteers who perform a wide range of tasks. Among other things, their responsibilities include developing the theming guidelines, designing the public convention space, setting up AV equipment for decorative purposes, furnishing the public convention space, and developing the wayfinding system in collaboration with other departments. <br />
+    We aim to create a pleasant and theme-specific atmosphere for our guests.<br />
+    To this end, we are looking for another lighting technician to join our existing team and help us create an unforgettable experience for our attendees. As part of our team, you will primarily work alongside our current technician to plan, set up, program, and dismantle the lighting design.
+</p>
+
+<h3>Your Responsibilities</h3>
+<ul>
+    <li>Planning and developing lighting concepts for the public convention space</li>
+    <li>Providing expert support to team members in the development of concepts</li>
+    <li>Setting up the decorative lighting equipment on Monday and Tuesday before the convention</li>
+    <li>Making minor adjustments or fixes during the convention</li>
+    <li>Tearing down the decorative lighting equipment on Sunday after the convention</li>
+</ul>
+
+<h3>Your Profile (Professional)</h3>
+<ul>
+    <li>You are interested in professional light tech</li>
+    <li>You have (some) experience with "CMRX" and "DMX" based light systems</li>
+    <li>You have (some) experience in using common lighting consoles (e.g., grandMa, HOG, or similar)</li>
+</ul>
+
+<h3>Your Profile (Personal)</h3>
+<ul>
+    <li>Proficiency in English; German is also welcome as an additional language.</li>
+    <li>You know that communication is important for our job.</li>
+    <li>You are willing to take the initiative and can handle tasks within your area of expertise.</li>
+    <li>You work closely with the team, coordinate with them and keep your colleagues up to date.</li>
+    <li>You will approach your team or colleagues in other departments if you encounter problems or need help.</li>
+    <li>You can stay professional and keep a cool head even under pressure.</li>
+    <li>You keep track of your deadlines and do your best to meet them.</li>
+</ul>
+
+<h3>Nice-to-Haves</h3>
+<ul>
+    <li>Ideally, you should have completed a vocational training program as an event technology specialist.</li>
+    <li>Aerial Work Platform Operator's License is desirable (IPAF 1a, 3a, 3b)(DGUV 308-008)</li>
+</ul>
+
+<p>
+    Interested in volunteering or have questions?<br />
+    Please contact us at: <a href="mailto:theming@eurofurence.org">theming@eurofurence.org</a>.<br />
+    We'll get back to you as soon as possible.
+</p>
