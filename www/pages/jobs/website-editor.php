1d852caf2a1076337d950e7fa4caaca5d8c27e57
Flam <flam@dogpixels.net>
2026-07-26 13:09:38 +0200

    jobs: add website-editor

---
+++ www/pages/jobs/website-editor.php
@@ -0,0 +1,28 @@
+<?php
+    $frontmatter["department"] = "Website";
+    $frontmatter["title"] = "Content Editor";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>"<span class="uk-text-italic">Wouldn't it be great if Eurofurence had this one function on their website?</span>" &ndash; If that's something you have thought before, this one might be for you. The Eurofurence Website Team is changing its mode of operation and seeks to distribute the content moderation and editing onto more shoulders. That's where you come in.</p>
+
+<p>In this volunteer job, you will be assigned a handful of this very website's sub-pages to administer. The content comes from the responsible department within Eurofurence in some text form, and you pour that into a HTML structure. For a live example, check out <a href="https://github.com/eurofurence/website/blob/main/www/pages/jobs/website-editor.php" target="_blank">this very job announcement's code</a>: this is an example of what you will be dealing with. Don't be alarmed by the first few rows of PHP code or the file being hosted on GitHub: if these things are alien to you, you will be provided with enough support to simply use them, without having to deal with them.
+
+<h3>What we expect of you:</h3>
+<ul>
+    <li><strong>Basic HTML and CSS knowledge</strong>: You edit contents within a provided, basic framework close to static HTML and CSS, therefore, you should know your way around the fundamental work with HTML and CSS. While the website is further powered by PHP and JavaScript, you will not be expected to handle any of that. Of course, understanding of PHP and JS would be a plus, or this can be acquired with time.</li>
+    <li><strong>Long-term membership</strong>: while assignments may vary, we prefer a stable team over high fluctuations. This is not to completely rule out changes of heart and situation, of course, but to signal that this assignment is not suitable to "peek in real quick and be gone by the next year".</li>
+    <li><strong>365 days of standby</strong>: Sounds scary, doesn't it? Don't worry: the website is being maintained all year long by nature (as opposed to many other departments that hibernate until shortly before the convention), thus updates can come in at any time throughout the year, and you will be expected to deal with them in a timely fashion. However, of course no one will blame you for putting your work, life, vacation and weekends first. updates <span class="uk-text-italic">during</span> the convention are quite rare, however.</li>
+    <li><strong>Commitment</strong>: You assume responsibility for the resorts under your control. Staff <strong>and</strong> Guests depend on you. Because our staffers tend to help at multiple departments and thin out their time and efforts across all jobs taken upon themselves, please consider whether you will be able to handle this job without burdening yourself.</li>
+    <li><strong>Autonomy</strong>: You will be expected to communicate with the department behind your assigned pages and perform edits and layouting yourself. The team of editors of other pages and of course your team lead, former editor and mentor will be there for you with support and backing, but you will be expected to carry out most of your duties yourself, with all the responsibility that comes with that. This is a volunteer job to pride yourself with, knowing you have contributed something to the grand event.</li>
+    <li><strong>Insight into the convention's inner working</strong>: While Eurofurence is strictly organized into departments, you will get to glimpse beyond those boundaries and get a feel of how the convention works on the inside.</li>
+</ul>
+
+<h3>What you can expect of us:</h3>
+<ul>
+    <li><strong>Create a piece of history</strong>: The websites we create are not just fire-and-forget informational pages. Upon their lifecycle's end, all pages wander into our <a href="https://archive.eurofurence.org" target="_blank">Archive</a>, to be saved for as long as Eurofurence exists, possibly even beyond that. Your name will be imprinted along these documents of history.</li>
+    <li><strong>Support</strong>: One point of this new team of editors is mutual support and improvement. With the requirements as low as knowing basic HTML and CSS, this assignment is ideal for beginners seeking to accumulate knowledge and experience in further web development.</li>
+    <li><strong>Staff status</strong>: Provided a passed probation and a certain amount of workload, you will be considered staff. You get to wear an orange lanyard and carry the burden of expected examplary behavior. There is, however, no reimbursement and no discounts associated with any staff status.</li>
+</ul>
+
+<p>We hope this has given you a good overview of the job at hand. If you still believe you're up to the task, we welcome you with open arms. Please hit up <a href="https://t.me/draconigen" target="_blank">@draconigen on Telegram</a> and let's talk.</p>
