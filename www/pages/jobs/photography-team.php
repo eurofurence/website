97f3cb50b17b1ec019af5bd77f46e0142eec44a0
Flam <flam@dogpixels.net>
2026-07-26 13:15:20 +0200

    jobs: add photography team

---
+++ www/pages/jobs/photography-team.php
@@ -0,0 +1,36 @@
+<?php
+    $frontmatter["department"] = "Photography";
+    $frontmatter["title"] = "Team Members";
+    echo '<h2>' . $frontmatter["department"] . ' &ndash; ' . $frontmatter["title"] . '</h2>';
+?>
+
+<p>The Photography Team is looking for help running the Fursuit Photoshoot!</p>
+<p>We are looking for photographers, assistants, and scene builders.</p>
+<p>Just like for EF29 with the space station, we are looking for skilled and creative folks willing to create one-of-a-kind photos during the convention. Our goal is to allow as many fursuiters as possible to get their awesome souvenir shot, so the task will be repetitive and we offer to take marvelous pictures within a two minute time slot per Fursuiter in our photo set.</p>
+<p>General Requirements:</p>
+<ul>
+    <li>Speak English/German</li>
+    <li>Be ready to help 3-4 hours per convention day</li>
+    <li>Buildup, teardown, operation is from Wednesday -> Sunday.</li>
+    <li>Have fun with the photography topic or working with fursuiters</li>
+</ul>
+<p>Task of Photographers:</p>
+<ul>
+    <li>Take photos during our opening times (equipment will be provided)</li>
+    <li>Edit the photos during and after the con (Software can be provided, own computer required)</li>
+    <li>Assist in buildup and teardown of the pre-built photo set</li>
+</ul>
+<p>Task of Assistants:</p>
+<ul>
+    <li>Be the person to keep track of timeslots and Fursuiters</li>
+    <li>Assist Fursuiters and photographers during the photoshoot</li>
+    <li>Explain the process to Fursuiters</li>
+    <li>Help pose the suiters and bring their characters to life. Creativity is highly welcome!</li>
+    <li>Assist in buildup and teardown of the pre-built photo set</li>
+</ul>
+<p>Task of Scene Builders:</p>
+<ul>
+    <li>Design and pre-build the photo set according to the year's convention theme</li>
+    <li>Lead the buildup and teardown of the photo set</li>
+</ul>
+<p>Interested?<br />Please contact us at <a href="mailtophoto@eurofurence.org">photo@eurofurence.org</a>!</p>
